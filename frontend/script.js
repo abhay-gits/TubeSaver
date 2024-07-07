e6c57191294ebef0bb9c11be728a906ab79383ee
Abhay Rana <anabhay42@gmail.com>
2024-07-07 18:12:45 +0530

    update

--- frontend/script.js
+++ frontend/script.js
@@ -1,7 +1,7 @@
 function sendData() {
     let Url = document.getElementById('link');
     if (Url.value) {
-        window.location.href = (`http://localhost:5173/download?link=${encodeURIComponent(Url.value)}`)
+        window.location.href = (`https://tubesaver-lmop.onrender.com/download?link=${encodeURIComponent(Url.value)}`)
         Url.value = "";
     } else {
         console.log("Youtube Url Required")
